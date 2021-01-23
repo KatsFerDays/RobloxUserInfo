a7e68cc3cb63f577f156c5261f244d192821384c
KatsFerDays <N/A>
2021-01-22 19:12:52 -0500

    Added the option to get user by username

--- RobloxUserInfo/Source.cpp
+++ RobloxUserInfo/Source.cpp
@@ -5,18 +5,61 @@
 #include <json/json.h>
 
 int main() {
+	int select;
+	std::string userID;
+	std::string username;
+	bool usernameError = false;
+	int endSelect;
 	while (true) {
-		std::string userID;
-		std::cout << "Please enter a UserID: ";
-		std::cin >> userID;
+		std::cout << "Select input type" << std::endl << "[U]sername" << std::endl << "User[I]D" << std::endl;
+		do {
+			select = _getch();
+		} while ((select != 'u') && (select != 'i'));
+		system("cls");
+
+		if (select == 'i') {
+			std::cout << "Please enter a UserID: ";
+			std::cin >> userID;
+		}
+		else if (select == 'u') {
+			std::cout << "Please enter a Username: ";
+			std::cin >> username;
+			system("cls");
+			cpr::Response nameToID = cpr::Get(cpr::Url{ "https://api.roblox.com/users/get-by-username?username=" + username });
+			Json::Value nameToIDParse;
+			std::istringstream nameToIDStr(nameToID.text);
+			nameToIDStr >> nameToIDParse;
+			if (nameToIDParse.isMember("errorMessage")) {
+				std::cout << nameToIDParse["errorMessage"].asString() << std::endl;
+				usernameError = true;
+				system("pause");
+				system("cls");
+			}
+			else if (nameToIDParse.isMember("success")) {
+				if (nameToIDParse["success"].asBool() == false) {
+					std::cout << "Unknown error with username to UserID API" << std::endl;
+					usernameError = true;
+					system("pause");
+					system("cls");
+				}
+			}
+			else {
+				userID = nameToIDParse["Id"].asString();
+			}
+		}
+
 		while (true) {
+			if (usernameError == true) {
+				break;
+			}
 			system("cls");
 			cpr::Response userInfo = cpr::Get(cpr::Url{ "https://users.roblox.com/v1/users/" + userID });
 			cpr::Response onlineStat = cpr::Get(cpr::Url{ "https://api.roblox.com/users/" + userID + "/onlinestatus/" });
 			if (onlineStat.status_code == 404 || userInfo.status_code == 404) {
 				std::cout << "Error 404: Page not found\nMost likely an incorrect UserID" << std::endl;
 				system("pause");
-				return 0;
+				system("cls");
+				break;
 			}
 			Json::Value userInfoParse;
 			std::istringstream userInfoStr(userInfo.text);
@@ -30,16 +73,15 @@ int main() {
 			std::cout << "GameID: " + onlineStatParse["GameID"].asString() << std::endl << "PlaceID: " + onlineStatParse["PlaceID"].asString() << std::endl << "UniverseID: " + onlineStatParse["UniverseID"].asString() << std::endl << "IsOnline: " + onlineStatParse["IsOnline"].asString() << std::endl << "LastOnline: " + onlineStatParse["LastOnline"].asString() << std::endl << "LastLocation: " + onlineStatParse["LastLocation"].asString() << std::endl << "LocationType: " + onlineStatParse["LocationType"].asString() << std::endl << "PresenceType: " + onlineStatParse["PresenceType"].asString() << std::endl << std::endl;
 			
 			std::cout << "[R]efresh" << std::endl << "[N]ew User" << std::endl << "[Q]uit" << std::endl;
-			int selection;
 			do {
-				selection = _getch();
-			} while ((selection != 'r') && (selection != 'n') && (selection != 'q'));
+				endSelect = _getch();
+			} while ((endSelect != 'r') && (endSelect != 'n') && (endSelect != 'q'));
 			
-			if (selection == 'n') {
+			if (endSelect == 'n') {
 				system("cls");
 				break;
 			}
-			else if (selection == 'q') {
+			else if (endSelect == 'q') {
 				return 0;
 			}
 		}
