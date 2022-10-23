66ab5b3d4b65730ff54b9c881aec43ee1121dfa1
KatsFerDays <N/A>
2022-10-23 15:52:14 -0400

    IT WAS THE F*CKING CACHE

--- xmake.lua
+++ xmake.lua
@@ -1,6 +1,9 @@
-add_requires("cpr", "nlohmann_json")
+add_repositories("private-xrepo https://github.com/KatsFerDays/private-xrepo.git")
+add_requires("cpr", "nlohmann_json", "librbxuserinfo")
+add_rules("mode.release", "mode.debug")
+set_languages("c++20")
 
 target("RobloxUserInfo")
     set_kind("binary")
     add_files("src/main.cpp")
-    add_packages("cpr", "nlohmann_json")
+    add_packages("cpr", "nlohmann_json", "librbxuserinfo")
